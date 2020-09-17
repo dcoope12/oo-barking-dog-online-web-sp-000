f58e3b4ff92fcb8c417697bdb06f0361266b3d26
Devin Cooper <dcoope12@asu.edu>
2020-09-17 21:31:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+    
+    def name
+      @this_dogs_name
+    end
+
+    def bark
+      puts "woof!"
+    end
+end
+
+4. Write a method, `.bark`, that `puts` "woof!" when called on an instance of Dog. 
